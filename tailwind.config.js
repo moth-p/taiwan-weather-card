38c36a5351f2a9ec6e0afcc4a66baeaa7ddb1639
Polly Hsu <pollylearnhsu@gmail.com>
2025-02-02 22:45:08 +0800

    feat: upload files

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./*.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+      extend: {
+        fontFamily: {
+          
+        },
+      },
+    },
+    plugins: [],
+  };
+  
